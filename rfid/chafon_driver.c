3cf3115855059f3147017d57ba6d848bd9515475
Mateusz Urbańczyk <urbanczyk@google.com>
2024-04-17 10:10:10 +0200

    Checkpatch corrections

--- rfid/chafon_driver.c
+++ rfid/chafon_driver.c
@@ -1,3 +1,10 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Driver for Chafon RFID tags reader
+ * Copyright (c) 2024
+ * Mateusz Urbańczyk <mateusz.urbanczyk97@gmail.com>
+ */
+
 #include <linux/fs.h>
 #include <linux/file.h>
 #include <linux/uaccess.h>
@@ -14,195 +21,189 @@
 
 #define READ_BUFFER_SIZE 512
 
-typedef enum {
-  IOCTL_SET_FILE = _IO('d', 0x01),
-  IOCTL_SET_SCAN_TIME = _IO('d', 0x02),
-  IOCTL_SET_POWER = _IO('d', 0x03),
-  IOCTL_SET_BUZZER = _IO('d', 0x04)
-} IoctlCmd;
+enum ioctl_cmd {
+	IOCTL_SET_FILE = _IO('d', 0x01),
+	IOCTL_SET_SCAN_TIME = _IO('d', 0x02),
+	IOCTL_SET_POWER = _IO('d', 0x03),
+	IOCTL_SET_BUZZER = _IO('d', 0x04)
+};
 
-typedef struct {
-  struct miscdevice misc;
-  struct fd dev_fd;
-} rfid_reader_t;
+struct rfid_reader {
+	struct miscdevice misc;
+	struct fd dev_fd;
+};
 
-int set_driver_file(rfid_reader_t *priv, int fd) {
-  if (priv->dev_fd.file)
-    fdput(priv->dev_fd);
+int set_driver_file(struct rfid_reader *priv, int fd)
+{
+	if (priv->dev_fd.file)
+		fdput(priv->dev_fd);
 
-  priv->dev_fd = fdget(fd);
+	priv->dev_fd = fdget(fd);
 
-  if (!priv->dev_fd.file)
-    return -EBADF;
+	if (!priv->dev_fd.file)
+		return -EBADF;
 
-  return 0;
+	return 0;
 }
 
-static long rfid_reader_ioctl(
-    struct file *file,
-    unsigned int cmd,
-    unsigned long user_arg
-) {
-  int kernel_arg, ret;
-  rfid_reader_t *priv;
+static long rfid_reader_ioctl(struct file *file,
+			      unsigned int cmd,
+			      unsigned long user_arg)
+{
+	int kernel_arg, ret;
+	struct rfid_reader *priv;
 
-  printk(KERN_NOTICE "rfid_reader_ioctl: cmd=%u, arg=%lu\n", cmd, user_arg);
+	printk(KERN_NOTICE "[%s]: cmd=%u, arg=%lu\n", __func__, cmd, user_arg);
 
-  priv = container_of(file->private_data, rfid_reader_t, misc);
+	priv = container_of(file->private_data, struct rfid_reader, misc);
 
-  ret = copy_from_user(&kernel_arg, (int __user *) user_arg, sizeof(int));
+	ret = copy_from_user(&kernel_arg, (int __user *)user_arg, sizeof(int));
 
-  if (ret) {
-    printk(KERN_NOTICE "ioctl failed copying from user: %d\n", ret);
-    return -EFAULT;
-  }
+	if (ret) {
+		printk(KERN_NOTICE "[%s] ioctl failed copying from user: %d\n", __func__, ret);
+		return -EFAULT;
+	}
 
-  switch (cmd) {
-    case IOCTL_SET_FILE:
-      ret = set_driver_file(priv, kernel_arg);
-      if (ret != 0)
-        return ret;
-      break;
-    case IOCTL_SET_SCAN_TIME:
-      ret = set_scan_time(priv->dev_fd.file, (uint8_t) kernel_arg);
-      if (ret != 0)
-        return ret;
-      break;
-    case IOCTL_SET_POWER:
-      ret = set_power(priv->dev_fd.file, (uint8_t) kernel_arg);
-      if (ret != 0)
-        return ret;
-      break;
-    case IOCTL_SET_BUZZER:
-      ret = set_buzzer(priv->dev_fd.file, (uint8_t) kernel_arg);
-      if (ret != 0)
-        return ret;
-      break;
-    default:
-      return -EINVAL;
-  }
+	switch (cmd) {
+	case IOCTL_SET_FILE:
+		ret = set_driver_file(priv, kernel_arg);
+		if (ret != 0)
+			return ret;
+		break;
+	case IOCTL_SET_SCAN_TIME:
+		ret = set_scan_time(priv->dev_fd.file, (uint8_t)kernel_arg);
+		if (ret != 0)
+			return ret;
+		break;
+	case IOCTL_SET_POWER:
+		ret = set_power(priv->dev_fd.file, (uint8_t)kernel_arg);
+		if (ret != 0)
+			return ret;
+		break;
+	case IOCTL_SET_BUZZER:
+		ret = set_buzzer(priv->dev_fd.file, (uint8_t)kernel_arg);
+		if (ret != 0)
+			return ret;
+		break;
+	default:
+		return -EINVAL;
+	}
 
-  return 0;
+	return 0;
 }
 
+static ssize_t rfid_reader_read(struct file *file,
+				char __user *user_buff,
+				size_t size,
+				loff_t *offset)
+{
+	struct rfid_reader *priv;
+	ssize_t num_bytes_read;
+	char kernel_buffer[READ_BUFFER_SIZE];
+	int code = translate_antenna_num(8);
 
-static ssize_t rfid_reader_read(
-    struct file *file,
-    char __user *user_buff,
-    size_t size,
-    loff_t *offset
-) {
-  rfid_reader_t *priv;
-  ssize_t num_bytes_read;
-  char kernel_buffer[READ_BUFFER_SIZE];
-  int code = translate_antenna_num(8);
+	printk(KERN_NOTICE "[%s]: %d\n", __func__, code);
 
-  printk(KERN_NOTICE "rfid_reader_read %d\n", code);
+	priv = container_of(file->private_data, struct rfid_reader, misc);
 
-  priv = container_of(file->private_data, rfid_reader_t, misc);
+	if (!priv->dev_fd.file)
+		return -ENODEV;
 
-  if (!priv->dev_fd.file)
-      return -ENODEV;
+	if (size > READ_BUFFER_SIZE) {
+		printk(KERN_NOTICE "[%s]: reading too much: %d\n", __func__, size);
+		return -ENOMEM;
+	}
 
-  if (size > READ_BUFFER_SIZE) {
-      printk(KERN_NOTICE "reading too much: %d\n", size);
-      return -ENOMEM;
-  }
+	read_tags(priv->dev_fd.file);
 
-  read_tags(priv->dev_fd.file);
+	num_bytes_read = 0;
 
-  num_bytes_read = 0;
+	if (num_bytes_read < 0)
+		return num_bytes_read;
 
-  if (num_bytes_read < 0)
-    return num_bytes_read;
+	printk(KERN_NOTICE "[%s]: buffer:\n%s\n", __func__, kernel_buffer);
 
-  printk(KERN_NOTICE "buffer:\n%s\n", kernel_buffer);
+	if (copy_to_user(user_buff, kernel_buffer, num_bytes_read))
+		return -EFAULT;
 
-  if (copy_to_user(user_buff, kernel_buffer, num_bytes_read))
-      return -EFAULT;
-
-  return num_bytes_read;
+	return num_bytes_read;
 }
 
-static int rfid_reader_release(struct inode *inode, struct file *file) {
-  rfid_reader_t *priv;
+static int rfid_reader_release(struct inode *inode, struct file *file)
+{
+	struct rfid_reader *priv;
 
-  printk(KERN_NOTICE "rfid_reader_release\n");
+	priv = container_of(file->private_data, struct rfid_reader, misc);
 
-  priv = container_of(file->private_data, rfid_reader_t, misc);
+	if (priv->dev_fd.file)
+		fdput(priv->dev_fd);
 
-  if (priv->dev_fd.file)
-    fdput(priv->dev_fd);
-
-  file->private_data = NULL;
-  return 0;
+	file->private_data = NULL;
+	return 0;
 }
 
-static int rfid_reader_open(struct inode *inode, struct file *file) {
-  printk(KERN_NOTICE "rfid_reader_open\n");
-
-  return 0;
+static int rfid_reader_open(struct inode *inode, struct file *file)
+{
+	// noop
+	return 0;
 }
 
 const struct file_operations rfid_reader_fops = {
-  .owner = THIS_MODULE,
-  .read = rfid_reader_read,
-  .open = rfid_reader_open,
-  .release = rfid_reader_release,
-  .unlocked_ioctl = rfid_reader_ioctl,
+	.owner = THIS_MODULE,
+	.read = rfid_reader_read,
+	.open = rfid_reader_open,
+	.release = rfid_reader_release,
+	.unlocked_ioctl = rfid_reader_ioctl,
 };
 
-static int rfid_reader_probe(struct platform_device *pdev) {
-  struct device *dev = &pdev->dev;
-  rfid_reader_t *priv;
-  int ret;
+static int rfid_reader_probe(struct platform_device *pdev)
+{
+	struct device *dev = &pdev->dev;
+	struct rfid_reader *priv;
+	int ret;
 
-  dev_err(dev, "rfid_reader_probe\n");
+	priv = devm_kzalloc(dev, sizeof(*priv), GFP_KERNEL);
+	if (!priv)
+		return -ENOMEM;
 
-  priv = devm_kzalloc(dev, sizeof(*priv), GFP_KERNEL);
-  if (!priv)
-    return -ENOMEM;
+	priv->misc.parent = dev;
+	priv->misc.name = "rfid-reader";
+	priv->misc.minor = MISC_DYNAMIC_MINOR;
+	priv->misc.fops = &rfid_reader_fops;
 
-  priv->misc.parent = dev;
-  priv->misc.name = "rfid-reader";
-  priv->misc.minor = MISC_DYNAMIC_MINOR;
-  priv->misc.fops = &rfid_reader_fops;
-
-  ret = misc_register(&priv->misc);
-  if (ret) {
-    dev_err(dev, "Unable to register misc device\n");
-    return ret;
-  }
+	ret = misc_register(&priv->misc);
+	if (ret) {
+		dev_err(dev, "Unable to register misc device\n");
+		return ret;
+	}
 
 	dev_set_drvdata(dev, priv);
 
-  return 0;
+	return 0;
 }
 
-static int rfid_reader_remove(struct platform_device *pdev) {
+static int rfid_reader_remove(struct platform_device *pdev)
+{
 	struct device *dev = &pdev->dev;
-  rfid_reader_t *priv = dev_get_drvdata(dev);
+	struct rfid_reader *priv = dev_get_drvdata(dev);
 
-  printk(KERN_NOTICE "rfid_reader_remove\n");
-
-  misc_deregister(&priv->misc);
-  return 0;
+	misc_deregister(&priv->misc);
+	return 0;
 }
 
 static const struct of_device_id rfid_reader_of_match[] = {
-  { .compatible = "rfid-reader" },
-  {},
+	{ .compatible = "rfid-reader" },
+	{},
 };
 MODULE_DEVICE_TABLE(of, rfid_reader_of_match);
 
-
 static struct platform_driver rfid_reader_driver = {
-  .driver = {
-    .name = "rfid-reader",
-    .of_match_table = rfid_reader_of_match,
-  },
-  .probe = rfid_reader_probe,
-  .remove = rfid_reader_remove,
+	.driver = {
+		.name = "rfid-reader",
+		.of_match_table = rfid_reader_of_match,
+	},
+	.probe = rfid_reader_probe,
+	.remove = rfid_reader_remove,
 };
 
 module_platform_driver(rfid_reader_driver);
--- rfid/reader.h
+++ rfid/reader.h
@@ -1,3 +1,4 @@
+/* SPDX-License-Identifier: GPL-2.0 */
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/fs.h>
@@ -8,45 +9,44 @@
 #define DEBUG_LOG_ENABLED 0
 
 // TODO: Expose all of them to the userspace via generic IOCTL_RUN_COMMAND
-typedef enum {
-    // Action commands
-    CMD_TAG_INVENTORY = 0x01,
-    CMD_READ_DATA = 0x02,
-    CMD_WRITE_DATA = 0x03,
-    CMD_WRITE_EPC = 0x04,
-    CMD_KILL_TAG = 0x05,
-    CMD_SET_PROTECTION = 0x06,
-    CMD_ERASE_BLOCK = 0x07,
-    CMD_READ_PROTECTION_EPC = 0x08,
-    CMD_READ_PROTECTION_NO_EPC = 0x09,
-    CMD_UNLOCK_READ_PROTECTION = 0x0a,
-    CMD_READ_PROTECTION_STATUS_CHECK = 0x0b,
-    CMD_EAS_CONFIGURATION = 0x0c,
-    CMD_EAS_ALERT_DETECTION = 0x0d,
-    CMD_SINGLE_TAG_INVENTORY = 0x0f,
-    CMD_WRITE_BLOCKS = 0x10,
-    CMD_GET_MONZA_4QT_WORKING_PARAMETERS = 0x11,
-    CMD_SET_MONZA_4QT_WORKING_PARAMETERS = 0x12,
-    CMD_READ_EXTENDED_DATA = 0x15,
-    CMD_WRITE_EXTENDED_DATA = 0x16,
-    CMD_TAG_INVENTORY_WITH_MEMORY_BUFFER = 0x18,
-    CMD_MIX_INVENTORY = 0x19,
-    CMD_INVENTORY_EPC = 0x1a,
-    CMD_QT_INVENTORY = 0x1b,
+enum cmd_type {
+	// Action commands
+	CMD_TAG_INVENTORY = 0x01,
+	CMD_READ_DATA = 0x02,
+	CMD_WRITE_DATA = 0x03,
+	CMD_WRITE_EPC = 0x04,
+	CMD_KILL_TAG = 0x05,
+	CMD_SET_PROTECTION = 0x06,
+	CMD_ERASE_BLOCK = 0x07,
+	CMD_READ_PROTECTION_EPC = 0x08,
+	CMD_READ_PROTECTION_NO_EPC = 0x09,
+	CMD_UNLOCK_READ_PROTECTION = 0x0a,
+	CMD_READ_PROTECTION_STATUS_CHECK = 0x0b,
+	CMD_EAS_CONFIGURATION = 0x0c,
+	CMD_EAS_ALERT_DETECTION = 0x0d,
+	CMD_SINGLE_TAG_INVENTORY = 0x0f,
+	CMD_WRITE_BLOCKS = 0x10,
+	CMD_GET_MONZA_4QT_WORKING_PARAMETERS = 0x11,
+	CMD_SET_MONZA_4QT_WORKING_PARAMETERS = 0x12,
+	CMD_READ_EXTENDED_DATA = 0x15,
+	CMD_WRITE_EXTENDED_DATA = 0x16,
+	CMD_TAG_INVENTORY_WITH_MEMORY_BUFFER = 0x18,
+	CMD_MIX_INVENTORY = 0x19,
+	CMD_INVENTORY_EPC = 0x1a,
+	CMD_QT_INVENTORY = 0x1b,
 
-    // Config commands
-    CF_GET_READER_INFO = 0x21,
-    CF_SET_WORKING_FREQUENCY = 0x22,
-    CF_SET_READER_ADDRESS = 0x24,
-    CF_SET_READER_INVENTORY_TIME = 0x25,
-    CF_SET_SERIAL_BAUD_RATE = 0x28,
-    CF_SET_RF_POWER = 0x2f,
-    CF_SET_WORK_MODE_288M = 0x76,
-    CF_SET_WORK_MODE_18 = 0x35,
-    CF_SET_BUZZER_ENABLED = 0x40,
-    CF_SET_ACCOUSTO_OPTIC_TIMES = 0x33
-} CmdType;
-
+	// Config commands
+	CF_GET_READER_INFO = 0x21,
+	CF_SET_WORKING_FREQUENCY = 0x22,
+	CF_SET_READER_ADDRESS = 0x24,
+	CF_SET_READER_INVENTORY_TIME = 0x25,
+	CF_SET_SERIAL_BAUD_RATE = 0x28,
+	CF_SET_RF_POWER = 0x2f,
+	CF_SET_WORK_MODE_288M = 0x76,
+	CF_SET_WORK_MODE_18 = 0x35,
+	CF_SET_BUZZER_ENABLED = 0x40,
+	CF_SET_ACCOUSTO_OPTIC_TIMES = 0x33
+};
 
 // Constants
 #define CMD_RESPONSE_BUFFER_SIZE 256
@@ -60,168 +60,162 @@ typedef enum {
 #define EPC_MAX_LEN 256
 
 #if LOG_ENABLED
-    #define LOG_ENTRY_LINE(...) \
-        do { \
-            printk(KERN_NOTICE "[%s:%d] %s: ", __FILE__, __LINE__, __func__); \
-        } while (0)
+	#define LOG_ENTRY_LINE(...) \
+		printk(KERN_NOTICE "[%s:%d] %s: ", __FILE__, __LINE__, __func__)
 #else
-    #define LOG_ENTRY_LINE(...) do { } while (0)
+	#define LOG_ENTRY_LINE(...) do { } while (0)
 #endif
 
 #if LOG_ENABLED
-    #define LOG_NO_NEWLINE(...) \
-        do { \
-            LOG_ENTRY_LINE(); \
-            printk(KERN_NOTICE __VA_ARGS__); \
-        } while (0)
+	#define LOG_NO_NEWLINE(...) \
+		do { \
+			LOG_ENTRY_LINE(); \
+			printk(KERN_NOTICE __VA_ARGS__); \
+		} while (0)
 #else
-    #define LOG_NO_NEWLINE(...) do { } while (0)
+	#define LOG_NO_NEWLINE(...) do { } while (0)
 #endif
 
 #if LOG_ENABLED
-    #define LOG(...) \
-        do { \
-            LOG_NO_NEWLINE(__VA_ARGS__); \
-            printk(KERN_NOTICE "\n"); \
-        } while (0)
+	#define LOG(...) \
+		do { \
+			LOG_NO_NEWLINE(__VA_ARGS__); \
+			printk(KERN_NOTICE "\n"); \
+		} while (0)
 #else
-    #define LOG(...) do { } while (0)
+	#define LOG(...) do { } while (0)
 #endif
 
 #if LOG_ENABLED
-    #define SECTION_LOG(...) \
-        do { \
-            LOG_ENTRY_LINE(); \
-            printk(KERN_NOTICE "-----"); \
-            printk(KERN_NOTICE __VA_ARGS__); \
-            printk(KERN_NOTICE "-----"); \
-            printk(KERN_NOTICE "\n"); \
-        } while (0)
+	#define SECTION_LOG(...) \
+		do { \
+			LOG_ENTRY_LINE(); \
+			printk(KERN_NOTICE "-----"); \
+			printk(KERN_NOTICE __VA_ARGS__); \
+			printk(KERN_NOTICE "-----"); \
+			printk(KERN_NOTICE "\n"); \
+		} while (0)
 #else
-    #define SECTION_LOG(...) do { } while (0)
+	#define SECTION_LOG(...) do { } while (0)
 #endif
 
 #if LOG_ENABLED
-    #define QUIET_LOG(...) \
-        do { \
-            printk(KERN_NOTICE __VA_ARGS__); \
-        } while (0)
+	#define QUIET_LOG(...) printk(KERN_NOTICE __VA_ARGS__)
 #else
-    #define QUIET_LOG(...) do { } while (0)
+	#define QUIET_LOG(...) do { } while (0)
 #endif
 
 #define SHOW_BUFFER_CONTENTS(buffer, size) \
-    do { \
-        for (ssize_t i = 0; i < size; i++) { \
-            QUIET_LOG("%02X ", (buffer)[i]); \
-        } \
-    } while (0)
+	do { \
+		for (ssize_t i = 0; i < (size); i++) { \
+			QUIET_LOG("%02X ", (buffer)[i]); \
+		} \
+	} while (0)
 
 #define SHOW_BUFFER(action, buffer, size) \
-    do { \
-        LOG_NO_NEWLINE("%s %zd bytes: ", action, size); \
-        SHOW_BUFFER_CONTENTS((buffer), size); \
-        QUIET_LOG("\n"); \
-    } while (0)
+	do { \
+		typeof(size) _size = size; \
+		LOG_NO_NEWLINE("%s %zd bytes: ", action, _size); \
+		SHOW_BUFFER_CONTENTS((buffer), _size); \
+		QUIET_LOG("\n"); \
+	} while (0)
 
+struct reader_command {
+	u8 addr;
+	enum cmd_type cmd;
+	u8 size;
+	u8 *data;
+};
 
-typedef struct {
-  uint8_t addr;
-  CmdType cmd;
-  uint8_t size;
-  uint8_t *data;
-} reader_command_t;
+struct reader_response {
+	u8 size;
+	u8 reader_addr;
+	u8 resp_cmd;
+	u8 status;
+	u8 *data;
+};
 
-typedef struct {
-  uint8_t size;
-  uint8_t reader_addr;
-  uint8_t resp_cmd;
-  uint8_t status;
-  uint8_t *data;
-} reader_response_t;
+struct inventory_tag {
+	u8 rssi;
+	u8 epc_len;
+	u8 epc[EPC_MAX_LEN];
+};
 
-typedef struct {
-  uint8_t rssi;
-  uint8_t epc_len;
-  uint8_t epc[EPC_MAX_LEN];
-} inventory_tag_t;
-
-typedef struct {
-  uint8_t antenna;
-  uint8_t num_tags;
-  inventory_tag_t **tags;
-} inventory_data_t;
+struct inventory_data {
+	u8 antenna;
+	u8 num_tags;
+	struct inventory_tag **tags;
+};
 
 #define SHOW_READER_COMMAND(cmd) \
-    do { \
-        LOG("<ReaderCommand: addr=0x%02X, cmd=0x%02X, size=0x%02X >", \
-            (cmd)->addr, (cmd)->cmd, (cmd)->size); \
-        if ((cmd)->size) { \
-          SHOW_BUFFER("Reader command data has", (cmd)->data, (ssize_t) (cmd)->size); \
-        } \
-    } while (0)
-
+	do { \
+		typeof(cmd) _cmd = cmd; \
+		LOG("<ReaderCommand: addr=0x%02X, cmd=0x%02X, size=0x%02X >", \
+			(_cmd)->addr, (_cmd)->_cmd, (_cmd)->size); \
+		if ((_cmd)->size) { \
+			SHOW_BUFFER("Reader command data has", \
+				    (_cmd)->data, \
+				    (ssize_t)(_cmd)->size); \
+		} \
+	} while (0)
 
 #define SHOW_READER_RESPONSE(cmd) \
-    do { \
-        LOG("<ReaderResponse: size=0x%02X, reader_addr=0x%02X, resp_cmd=0x%02X, status=0x%02X >", \
-            (cmd)->size, (cmd)->reader_addr, (cmd)->resp_cmd, (cmd)->status); \
-        if ((cmd)->size) { \
-          SHOW_BUFFER("Received response of", (cmd)->data, (ssize_t) (cmd)->size); \
-        } \
-    } while (0)
-
+	do { \
+		typeof(cmd) _cmd = cmd; \
+		LOG("<ReaderResponse: size=0x%02X, addr=0x%02X, resp_cmd=0x%02X, status=0x%02X >", \
+			(_cmd)->size, (_cmd)->reader_addr, (_cmd)->resp__cmd, (_cmd)->status); \
+		if ((_cmd)->size) { \
+			SHOW_BUFFER("Received response of", (_cmd)->data, (ssize_t)(_cmd)->size); \
+		} \
+	} while (0)
 
 #define SHOW_INVENTORY_TAG(tag) \
-    do { \
-        typeof(tag) _tag = tag; \
-        if (searched_epc_len > 0 && \
-            (memcmp((_tag)->epc, searched_epc, searched_epc_len) == 0)) { \
-          QUIET_LOG("[*FOUND*]"); \
-        } \
-        QUIET_LOG("<Tag: "); \
-        QUIET_LOG("rssi=%u, ", (_tag)->rssi); \
-        QUIET_LOG("epc_len=%u, ", (_tag)->epc_len); \
-        QUIET_LOG("epc="); \
-        for (size_t i = 0; i < (_tag)->epc_len; ++i) { \
-           QUIET_LOG("%02X ", (_tag)->epc[i]); \
-        } \
-        QUIET_LOG(">\n"); \
-    } while (0)
+	do { \
+		typeof(tag) _tag = tag; \
+		if (searched_epc_len > 0 && \
+			(memcmp((_tag)->epc, searched_epc, searched_epc_len) == 0)) { \
+			QUIET_LOG("[*FOUND*]"); \
+		} \
+		QUIET_LOG("<Tag: "); \
+		QUIET_LOG("rssi=%u, ", (_tag)->rssi); \
+		QUIET_LOG("epc_len=%u, ", (_tag)->epc_len); \
+		QUIET_LOG("epc="); \
+		for (size_t i = 0; i < (_tag)->epc_len; ++i) { \
+			QUIET_LOG("%02X ", (_tag)->epc[i]); \
+		} \
+		QUIET_LOG(">\n"); \
+	} while (0)
 
 #define SHOW_INVENTORY_DATA(inv) \
-    do { \
-        typeof(inv) _inv = inv; \
-        QUIET_LOG("<Inventory Data: "); \
-        QUIET_LOG("Antenna=%u, ", (_inv)->antenna); \
-        QUIET_LOG("#Tags=%u>\n", (_inv)->num_tags); \
-        for (size_t i = 0; i < (_inv)->num_tags; ++i) { \
-            QUIET_LOG("[%zu] ", i + 1); \
-            SHOW_INVENTORY_TAG((_inv)->tags[i]); \
-        } \
-    } while (0)
-
+	do { \
+		typeof(inv) _inv = inv; \
+		QUIET_LOG("<Inventory Data: "); \
+		QUIET_LOG("Antenna=%u, ", (_inv)->antenna); \
+		QUIET_LOG("#Tags=%u>\n", (_inv)->num_tags); \
+		for (size_t i = 0; i < (_inv)->num_tags; ++i) { \
+			QUIET_LOG("[%zu] ", i + 1); \
+			SHOW_INVENTORY_TAG((_inv)->tags[i]); \
+		} \
+	} while (0)
 
 #define CRC_MSB(crc) ((crc) >> 8)
 #define CRC_LSB(crc) ((crc) & 0xFF)
 
-uint16_t crc_checksum(uint8_t *data, int len);
+uint16_t crc_checksum(u8 *data, int len);
 
-int write_frame(struct file *serial_file, reader_command_t *cmd);
+int write_frame(struct file *serial_file, struct reader_command *cmd);
 
 ssize_t read_frame(struct file *serial_file, uint8_t *buffer);
 
-int verify_checksum(uint8_t *data, ssize_t data_len, uint8_t *checksum_bytes);
+int verify_checksum(u8 *data, ssize_t data_len, uint8_t *checksum_bytes);
 
-int parse_frame(uint8_t *buffer, ssize_t buffer_size, reader_response_t *frame);
+int parse_frame(u8 *buffer, ssize_t buffer_size, struct reader_response *frame);
 
-void free_frame(reader_response_t *frame);
+void free_frame(struct reader_response *frame);
 
-int run_command(
-    struct file *serial_file,
-    reader_command_t *cmd,
-    reader_response_t *reader_resp
-);
+int run_command(struct file *serial_file,
+		struct reader_command *cmd,
+		struct reader_response *reader_resp);
 
 int set_buzzer(struct file *serial_file, uint8_t value);
 
@@ -231,11 +225,9 @@ int set_scan_time(struct file *serial_file, uint8_t value);
 
 int translate_antenna_num(int antenna_code);
 
-int parse_inventory_data(
-  reader_response_t *resp,
-  inventory_data_t *inventory
-);
+int parse_inventory_data(struct reader_response *resp,
+			 struct inventory_data *inventory);
 
-void free_inventory_data(inventory_data_t *inventory);
+void free_inventory_data(struct inventory_data *inventory);
 
 int read_tags(struct file *serial_file);
